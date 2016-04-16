67bb120c1616eb6172b50360c1911482353ec2f6
david.acero@outlook.com <david.acero@outlook.com>
2016-04-16 20:34:44 +1000

    Solutions for problems B and C. B still spends more than 1 second executing.

---
+++ ConsoleApplication1/B.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class B
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+
+            long s = long.Parse(input[0]);
+            long vOne = long.Parse(input[1]);
+            long vTwo = long.Parse(input[2]);
+
+            long x = 0;
+            long y = 0;
+
+            if (vOne >= vTwo)
+            {
+                x = vOne;
+                y = vTwo;
+            }
+            else
+            {
+                y = vOne;
+                x = vTwo;
+            }
+
+            long a = s / x;
+            int b = 0;
+
+            bool notFound = false;
+
+            while ((a * x) + (b * y) != s && !notFound)  
+            {
+                a--;
+                if (a == -1)
+                {
+                    notFound = true;
+                }
+                else
+                {
+                    b = 0;
+                    while ((b * y) < s - (a * x))
+                    {
+                        b++;
+                    }
+                }
+            }
+
+            if(notFound)
+            {
+                Console.WriteLine("Impossible");
+            }
+            else
+            {
+                Console.WriteLine(a + " " + b);
+            }
+
+            
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApplication1/C.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class C
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List <Person> people = new List<Person>();
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split(' ');
+                people.Add(new Person(input[0], input[1]));
+            }
+
+            people.Sort((x, y) => string.Compare(x.FullName, y.FullName));
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(people[i].FirstName + " " + people[i].LastName);
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+    class Person
+    {
+        private string firstName;
+        private string lastName;
+        private string fullName;
+
+        public Person(string first, string last)
+        {
+            firstName = first;
+            lastName = last;
+            fullName = last + " " + first;
+        }
+
+        public string FirstName
+        {
+            get
+            {
+                return firstName;
+            }
+
+            set
+            {
+                firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return lastName;
+            }
+
+            set
+            {
+                lastName = value;
+            }
+        }
+
+        public string FullName
+        {
+            get
+            {
+                return fullName;
+            }
+
+            set
+            {
+                fullName = value;
+            }
+        }
+    }
+}
